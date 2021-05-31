28fa5a8834728701510a5f242ca8ae5ca43b1b25
Maria Giovanna Donadio <giorgiavans@MBP-Queen-Giorgiavans.local>
2021-05-31 16:52:53 +0200

    creazione file Clion

---
+++ FileINI/cmake-build-debug/CMakeFiles/FileINI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileINI.dir/main.cpp.o"
+  "FileINI"
+  "FileINI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileINI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
